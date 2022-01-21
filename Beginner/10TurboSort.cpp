61df90a24334d0b2a0a242fbf6f77875981afe66
aniket <hexaoctalai@gmail.com>
2022-01-21 12:41:02 +0530

    commit

--- Beginner/10TurboSort.cpp
+++ Beginner/10TurboSort.cpp
@@ -1,5 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
 
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; ++i)
+    {
+        cin>>arr[i];
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout<<arr[i]<<endl;
+    }
+    
 }
+
